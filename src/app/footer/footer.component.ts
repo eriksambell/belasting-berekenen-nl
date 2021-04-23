bf1370e2257ac7aa85c5f04d8200b545fd78e464
ErikS <erik.sambell@incentro.com>
2021-04-23 19:36:25 +0200

    updated footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,15 +5,19 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
-  year = new Date().getFullYear();
+  year: number;
 
-  @Output() dataEvent = new EventEmitter<any>();
+  @Output() 
+  dataEvent = new EventEmitter<any>();
 
-  clickMenu(){
+  ngOnInit(): void {
+    this.year = new Date().getFullYear()
+  }
+
+  public onClickFooterMenuItem(): void {
     this.dataEvent.emit(undefined);
   }
 
-  constructor() { }
 }
