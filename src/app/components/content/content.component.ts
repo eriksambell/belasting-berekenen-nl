10a5bbe007de5f35ee0b0bb6739e4fbce6fab5e5
ErikS <erik.sambell@incentro.com>
2021-07-06 05:03:38 +0200

    basis upgrades to content component

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BUDGET, BudgetItem } from "src/app/shared/budget.constant";
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html'
+  selector: "app-content",
+  templateUrl: "./content.component.html",
+  styleUrls: ["./content.component.scss"],
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  revenue: number;
+  expenditure = 337;
+  year = 2021;
 
+  ngOnInit(): void {
+    this.revenue = Math.round(
+      BUDGET.map((item: BudgetItem) => item.amount).reduce(
+        (sum: number, current: number) => sum + current
+      )
+    );
+  }
 }
